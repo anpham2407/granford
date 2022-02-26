2c4280ed208d6ebfadfa4909bfcf273ba0710f7f
An Pham <an.pham@thuocsi.vn>
2022-02-26 15:14:32 +0700

    add new template

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -13,45 +13,48 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php if ( ! is_front_page() ) : ?>
-		<header class="entry-header alignwide">
-			<?php get_template_part( 'template-parts/header/entry-header' ); ?>
-			<?php twenty_twenty_one_post_thumbnail(); ?>
-		</header><!-- .entry-header -->
-	<?php elseif ( has_post_thumbnail() ) : ?>
-		<header class="entry-header alignwide">
-			<?php twenty_twenty_one_post_thumbnail(); ?>
-		</header><!-- .entry-header -->
-	<?php endif; ?>
+	<div class="container entry-fix-width">
 
-	<div class="entry-content">
-		<?php
-		the_content();
+		<?php if ( ! is_front_page() ) : ?>
+			<header class="entry-header alignwide">
+				<?php get_template_part( 'template-parts/header/entry-header' ); ?>
+				<?php twenty_twenty_one_post_thumbnail(); ?>
+			</header><!-- .entry-header -->
+		<?php elseif ( has_post_thumbnail() ) : ?>
+			<header class="entry-header alignwide">
+				<?php twenty_twenty_one_post_thumbnail(); ?>
+			</header><!-- .entry-header -->
+		<?php endif; ?>
 
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
-				'after'    => '</nav>',
-				/* translators: %: Page number. */
-				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer default-max-width">
+		<div class="entry-content">
 			<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post. Only visible to screen readers. */
-					esc_html__( 'Edit %s', 'twentytwentyone' ),
-					'<span class="screen-reader-text">' . get_the_title() . '</span>'
-				),
-				'<span class="edit-link">',
-				'</span>'
+			the_content();
+
+			wp_link_pages(
+				array(
+					'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+					'after'    => '</nav>',
+					/* translators: %: Page number. */
+					'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+				)
 			);
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+		</div><!-- .entry-content -->
+
+		<?php if ( get_edit_post_link() ) : ?>
+			<footer class="entry-footer default-max-width">
+				<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post. Only visible to screen readers. */
+						esc_html__( 'Edit %s', 'twentytwentyone' ),
+						'<span class="screen-reader-text">' . get_the_title() . '</span>'
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</footer><!-- .entry-footer -->
+		<?php endif; ?>
+	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
