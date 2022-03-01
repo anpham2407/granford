7480d2e30d806df47f395efbb3f6ffd215034257
An Pham <an.pham@thuocsi.vn>
2022-03-01 17:09:08 +0700

    update content

--- contentSingleTemplate.php
+++ contentSingleTemplate.php
@@ -15,7 +15,7 @@ get_header();
 /* Start the Loop */
 while ( have_posts() ) :
 	the_post();
-	get_template_part( 'template-parts/content/content-page' );
+	get_template_part( 'template-parts/content/content-single' );
 endwhile; // End of the loop.
 
 get_footer();
